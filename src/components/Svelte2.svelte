4c02e30ef81a405352eddeb3c057dd7d3618cd7e
taoguangc <taoguangc@gmail.com>
2025-05-02 22:57:25 +0800

    feat: 添加新的 Svelte2 组件到首页
    在首页添加了新的 Svelte2 组件，以展示更多 Svelte 的使用示例

---
+++ src/components/Svelte2.svelte
@@ -0,0 +1,26 @@
+<script>
+  let name = "John";
+  let isVisible = true;
+</script>
+
+<section>
+  <div class="container mx-auto">
+    <h1>Hello, world 2!</h1>
+    <p>Welcome to my Svelte app 2.</p>
+    <p>Here are some examples of how to use Svelte:</p>
+    <ul>
+      <li>
+        <strong>Variables:</strong> Use the syntax to display
+        variables. For example:
+        <p>Hello, {name}!</p>
+      </li>
+      <li>
+        <strong>Conditionals:</strong> Use the <code>#if</code> statement to
+        conditionally render elements. For example:
+        {#if isVisible}
+          <p>This text is visible!</p>
+        {/if}
+      </li>
+    </ul>
+  </div>
+</section>
